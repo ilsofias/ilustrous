3c0938e74ea4038ca35a06f6e2ff4fc60ec57a16
Sofia Ilustre <sofiasimoune.ilustre@gmail.com>
2025-07-29 18:33:50 +0800

    initial commit of front-end files

---
+++ front-end/index.js
@@ -0,0 +1,8 @@
+import { registerRootComponent } from 'expo';
+
+import App from './App'; // Adjust the import path if necessary
+import { name as appName } from './app.json'; // Adjust the import path if necessary    
+
+
+
+registerRootComponent(App);
